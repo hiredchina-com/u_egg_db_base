cb1e4ffc05bcf8d3a7a493b52bb8fdd2bb6b5951
Thomas Lau <thomas_0836@qq.com>
2018-11-28 17:20:08 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 const BaseControllerTest = require('./lib/baseControllerTest');
 const BaseServiceTest = require('./lib/baseServiceTest');
+const BaseService = require('./lib/baseService');
+const BaseController = require('./lib/baseController');
 
 module.exports = {
   BaseControllerTest,
-  BaseServiceTest
+  BaseServiceTest,
+  BaseController,
+  BaseService
 }
